fcd3aa0adf29fbfdf315e7996ae10dea83c4dd81
Luke-technolab <luke@technolableiden.nl>
2026-07-03 13:31:14 +0200

    forgot to update the file paths _-_

--- intergrations/Github Intergration/imports.js
+++ intergrations/Github Intergration/imports.js
@@ -1,9 +1,9 @@
-import { GithubIntergration } from './Github Intergration/GithubIntergration.js';
-import './Github Intergration/Elements/githubIssues.js';
-import './Github Intergration/Elements/githubRepos.js';
-import './Github Intergration/Elements/githubLogin.js';
-import './Github Intergration/Elements/github_mde_editor.js';
-import './Github Intergration/Elements/github_labels.js';
+import { GithubIntergration } from './GithubIntergration.js';
+import './Elements/githubIssues.js';
+import './Elements/githubRepos.js';
+import './Elements/githubLogin.js';
+import './Elements/github_mde_editor.js';
+import './Elements/github_labels.js';
 
 //make a startup call to the render service once the site has loaded
 let github_intergration = new GithubIntergration();
